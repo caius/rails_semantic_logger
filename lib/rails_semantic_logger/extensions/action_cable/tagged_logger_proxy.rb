0a88fd9c060b1de7eb2263a96f508e299bd52695
Caius Durling <dev@caius.name>
2020-08-23 15:25:10 +0100

    Compute tags correctly for ActionCable

--- lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
+++ lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
@@ -4,7 +4,7 @@ module ActionCable
   module Connection
     class TaggedLoggerProxy
       def tag(logger, &block)
-        current_tags = tags - logger.formatter.current_tags
+        current_tags = tags - logger.tags
         logger.tagged(*current_tags, &block)
       end
     end
